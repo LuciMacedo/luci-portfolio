a3c177d14092e7a21379d9fb8cbc42cb53dc7981
LuciMacedo <luci_macedom@hotmail.com>
2023-05-06 20:17:22 +0100

    update my profile and my resume

--- src/assets/components/header/Header.jsx
+++ src/assets/components/header/Header.jsx
@@ -10,8 +10,7 @@ export function Header() {
       <article>
         <strong>Hello! I am Luci</strong>
         <h1>Junior Software Engineer</h1>
-        <p>I like making online life easier with a touch of beauty. Back in 2020 when I decided to have a career change I started my learning journey. I am passionate about every line of code and proud of myself for every step of my progress.</p>
-        <p>A focused, results-driven, and highly capable Junior Software Engineer offering sound theoretical knowledge and practical experience across projects.</p>
+        <p>I am passionate about making online life easier with a touch of beauty. Back in 2020, I decided to embark on a career change and started my journey to become a software engineer. I take pride in every line of code and every step of progress I make. As a focused, results-driven, and highly capable Junior Software Engineer, I offer sound theoretical knowledge and practical experience across projects.</p>
         <button>
           <a href={lucicv} target="_blank" download>
             Download CV 
