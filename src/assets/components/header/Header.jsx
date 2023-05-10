1a2f1d8a5525286a01a8633ecc1c8c105090a3b8
LuciMacedo <luci_macedom@hotmail.com>
2023-05-10 23:35:52 +0100

    uptade resume

--- src/assets/components/header/Header.jsx
+++ src/assets/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import { HeaderWrapper } from "./style"
 import LuciPic from '../../img/luci5.webp'
 import { DownloadSimple } from "phosphor-react";
-import lucicv from '../../img/lucicv.pdf'
+import luciResume from '../../img/lucicv.pdf'
 
 export function Header() {
 
@@ -12,7 +12,7 @@ export function Header() {
         <h1>Junior Software Engineer</h1>
         <p>I am passionate about making online life easier with a touch of beauty. Back in 2020, I decided to embark on a career change and started my journey to become a software engineer. I take pride in every line of code and every step of progress I make. As a focused, results-driven, and highly capable Junior Software Engineer, I offer sound theoretical knowledge and practical experience across projects.</p>
         <button>
-          <a href={lucicv} target="_blank" download>
+          <a href={luciResume} target="_blank" download>
             Download CV 
             <DownloadSimple size={20}/>
           </a>
