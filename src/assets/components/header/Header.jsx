493261ff2d1e38a46170acf366bf38ffacd37680
LuciMacedo <luci_macedom@hotmail.com>
2023-05-30 15:03:35 +0100

    misspellings

--- src/assets/components/header/Header.jsx
+++ src/assets/components/header/Header.jsx
@@ -10,7 +10,7 @@ export function Header() {
       <article>
         <strong>Hello! I am Luci</strong>
         <h1>Junior Software Engineer</h1>
-        <p>I'm driven by a deep passion for simplifying the online world and making life easier for users. In 2020, I decided to pursue a career as an Software Engineer, and since then, I've poured my heart into every line of code I write and every milestone I achieve. As a dedicated and results-oriented Junior Software Engineer, I bring a unique blend of theoretical expertise and hands-on experience gained from working on various projects. I am eager to contribute my skills and knowledge to your team and help create innovative solutions that will leave a lasting impact.</p>
+        <p>I'm driven by a deep passion for simplifying the online world and making life easier for users. In 2020, I decided to pursue a career as a Software Engineer, and since then, I've poured my heart into every line of code I write and every milestone I achieve. As a dedicated and results-oriented Junior Software Engineer, I bring a unique blend of theoretical expertise and hands-on experience gained from working on various projects. I am eager to contribute my skills and knowledge to your team and help create innovative solutions that will leave a lasting impact.</p>
         <button>
           <a href={luciResume} target="_blank" download>
             Download CV 
